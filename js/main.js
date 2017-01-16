3268ad0b70b63d51c7aaee4fbc5059ba7173899c
N_Ireland <gfx@minigraphix.ca>
2017-01-16 13:51:08 -0500

    added some styling features, added mobile nav functionality and styling, fixed footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 (function(){
+var copy = document.querySelector("#copyToggle");//for copyright info toggle
+var hamburger = document.querySelector(".hamburger");
+function toggleCopy(event){
+  event.preventDefault();
+  var it = event.currentTarget;
+  it.parentNode.parentNode.querySelector('.center').classList.toggle('hide');
+}
+function toggleMenu(event){
+  event.preventDefault();
+  // var it = event.currentTarget;
+  document.querySelector("#mainNav").classList.toggle('closed');
 
+}
+copy.addEventListener('click',toggleCopy,false);
+hamburger.addEventListener('click',toggleMenu,false);
 })();
