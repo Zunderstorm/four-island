7b5d63767fe697df7da102fab9a29c42e0652fcd
N_Ireland <gfx@minigraphix.ca>
2017-01-17 10:53:30 -0500

    changing some caching things

---
+++ sw.js
@@ -0,0 +1,36 @@
+(function(){
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/main.css',
+  '/js/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+//END install
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          console.log("return" + response);
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+})();
